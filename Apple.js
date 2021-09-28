089f4043d2e7e1b11d708cec6d811a952b0c338c
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-09-28 10:32:52 +0700

    update

---
+++ Apple.js
@@ -0,0 +1,22 @@
+class Apple {
+    weight
+    constructor(weight) {
+        this.weight = weight
+    }
+    getWeight() {
+        return this.weight
+    }
+    setWeight(weight) {
+        this.weight = weight
+    }
+    decrease() {
+        this.weight -= 1
+    }
+    isEmpty() {
+        if (this.weight === 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
---
+++ Human.js
@@ -0,0 +1,11 @@
+class Human{
+    name
+    gender
+    weight
+    constructor(name, gender, weight) {
+        this.name = name
+        this.gender = gender
+        this.weight = weight
+    }
+    getName
+}
